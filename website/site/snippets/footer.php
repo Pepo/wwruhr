e20727548d248c4e2a35a90292c2e376d1b1755c
Pascal Szewczyk <ps@elektrowecker.de>
2015-01-25 23:04:00 +0100

    repair the asset stuff

--- website/site/snippets/footer.php
+++ website/site/snippets/footer.php
@@ -1,14 +1,30 @@
-  <footer class="footer cf" role="contentinfo">
-
-    <div class="copyright">
-      <?php echo $site->copyright()->kirbytext() ?>
+  </article>
+            <aside>
+                <h2>Location</h2>
+                <p>Wir treffen uns im Unperfekthaus in Essen</p>
+                <iframe width='319' height='319' frameBorder='0' src='http://a.tiles.mapbox.com/v3/derpepo.map-1c7zpsc7.html#16/51.45872669642711/7.007458806037903'></iframe>
+                <p>Haltestelle: Berliner Platz</p>
+                <ul>
+                    <li>U11, U17 und U18 (U-Bahn)</li>
+                    <li>101, 103, 105 und 109 (Tram)</li>
+                    <li>145, 147, 166 und SB16 (Bus)</li>
+                    <li>NE10, NE11, NE12 und NE16 (Nachtexpress)</li>
+                </ul>
+                <?php snippet('menu') ?>
+            </aside>
+        </section>
     </div>
+    <footer class="clearfix">
+        <h2>Willkommen im Ruhrgebiet,wo man Gesichter Fressen nennt! <span>(Frank Goosen)</span></h2>
+        <h3>Diese Menschen sind Teil der <a href="www.meetup.com/wwruhr/">Meetup.com-Gruppe Webworker Ruhr</a></h3>
+        <div class="ppl">
+            <?php $json = json_decode(file_get_contents("https://api.meetup.com/2/members?group_id=17613862&key=484c52501a463038165f2827e1175")) ?>
 
-    <div class="colophon">
-      <a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a>
-    </div>
-
-  </footer>
+            <?php foreach( $json->{'results'} as $ppl ): ?>
+                <a href="<?php print $ppl->link ?>"><?php print $ppl->name ?><?php if(isset($ppl->photo)): ?><img src="<?php print $ppl->photo->thumb_link; ?>"><?php endif; ?></a>
+            <?php endforeach; ?>
+        </div>
+    </footer>
 
 </body>
 </html>
--- website/site/snippets/header.php
+++ website/site/snippets/header.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
 
+  <title>Webworker Ruhr <?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
-  <?php echo css('assets/css/main.css') ?>
-
+  <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
+  <meta name="robots" content="index, follow" />
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans:400italic,400,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Donegal+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/assets/css/screen.css" />
 </head>
 <body>
+    <div id="wrapper">
+        <header>
+            <a href="/"><img src="/assets/images/logo.png" alt="Webworker Ruhr Logo"></a>
+            <h1>Webworker Ruhr</h1>
+            <aside>
+                <h2>Nächster Termin</h2>
+                <time datetime="2012-24-01T19:00+00:00">27. November 2014 - 19:00 Uhr</time>
 
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
+                <h3>Themen</h3>
+                <ul>
+                    <li>SOLID (object-oriented design)</li>
+                    <li>Bessere Logingformulare. "Login failed. Retry?"</li>
+                </ul>
+            </aside>
+        </header>
+        <nav>
+            <?php foreach($pages as $page): ?>
+                <a href="<?php print $page->url() ?>">
+                <?php print $page->title() ?></a>
+            <?php endforeach; ?>
+        </nav>
+
+
+        <section>
+        <article>
