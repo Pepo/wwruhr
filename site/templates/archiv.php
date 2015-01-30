41f486aa0db36e6aa225761697186a874ccd67b2
Frederic Hemberger <mail@frederic-hemberger.de>
2015-01-30 14:59:03 +0100

    Design proposal, fix archive view

--- site/templates/archiv.php
+++ site/templates/archiv.php
@@ -3,20 +3,14 @@
 
                 <h3>Was bisher geschah</h3>
 
-                <?php
-                    $archiv = $pages->find('archiv')->children()->flip();
-
-                ?>
+                <?php $archiv = $pages->find('archiv')->children()->flip(); ?>
                 <?php foreach($archiv as $a): ?>
                     <div class="event clearfix">
-                        <h4><?php print $a->h1()?> - <?= $a->datum() ?></h4>
-
-
-                        <?php $image =  $a->images()->first() ?>
-                        <?php echo thumb( $image, array('width' => 95, 'height' => 95, 'upscale' => true, 'crop' => true) ); ?>
+                        <h4><?php print $a->h1(); ?> - <?= $a->datum(); ?></h4>
+                        <?php $image = $a->images()->first(); ?>
+                        <?php if ($image) { echo thumb( $image, array('width' => 95, 'height' => 95, 'upscale' => true, 'crop' => true) ); } ?>
                         <p><?= kirbytext($a->shorttext()) ?></p>
                         <p><a href="<?php echo $a->url() ?>" class="right">Details</a></p>
-
                     </div>
                 <?php endforeach; ?>
 <?php snippet('footer') ?>
