ba7404d9c2e18aab0ed2b267f9ecffafb58230e4
Pascal Szewczyk <pascal@peppmedien.de>
2015-02-26 19:41:06 +0100

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -34,15 +34,16 @@
             <aside>
                 <h2>Nächster Termin</h2>
                 <time datetime="2015-03-26T19:00+00:00">26. März 2015 - 19:00 Uhr</time>
-<!--
+
                 <h3>Themen</h3>
                 
                 <ul>
-                    <li>SOLID (object-oriented design) mit Christian Schorn</li>
-                    <li>Christian Schaefer mit HTTP/2 the Future is now.</li>
+                    <li>IndieWeb Movement mit Nicolau Schwarz</li>
+
+                    <li>Noch ein weiteres Thema. Hast du vielleicht eine Idee? Einfach melden!</li>
                 </ul>
                 <p><a href="http://www.meetup.com/wwruhr/events/220209902/">Anmelden auf meetup.com/wwruhr</a></p>
--->
+
             </aside>
         </div>
     </header>
